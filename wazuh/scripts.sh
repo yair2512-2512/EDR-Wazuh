804cf30373ca0f7262cc4710133f2af0eed33e42
yair2512-2512 <ycohen522@gmail.com>
2025-01-05 21:44:01 +0200

    Update scripts.sh

--- wazuh/scripts.sh
+++ wazuh/scripts.sh
@@ -6,12 +6,25 @@ ufw reload
 echo "IP $IP_TO_BLOCK has been blocked."
 #######################################
 ###block_user.sh###
+#!/bin/bash
 username=$1
 if [ -z "$username" ]; then
-  echo "user not found."
+  echo "No username provided."
   exit 1
 fi
+ips=$(grep "Accepted password for $username" /var/log/auth.log | grep -oP 'from \K[\d\.]+' | sort | uniq)
+if [ -z "$ips" ]; then
+  echo "No successful connection found for user $username."
+  exit 1
+fi
+echo "The IPs for user $username are:"
+echo "$ips"
 sudo usermod -L $username
-echo "block $username ."
+echo "User $username has been locked."
+echo "Block $username."
 sudo pkill -KILL -u $username
+for ip in $ips; do
+  sudo iptables -A INPUT -s $ip -j DROP
+  echo "IP $ip has been blocked from connecting."
+done
 
