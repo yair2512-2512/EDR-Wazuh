ed2613107641a69d4cb1b96cec6f62f9674d47d1
yair2512-2512 <ycohen522@gmail.com>
2025-01-05 21:29:22 +0200

    Update scripts.sh

--- wazuh/scripts.sh
+++ wazuh/scripts.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
+###block-ip.sh###
 IP_TO_BLOCK=$1
 ufw deny from $IP_TO_BLOCK
 ufw reload
 echo "IP $IP_TO_BLOCK has been blocked."
-###block-ip.sh###
-
+#################################
+username=$1
+if [ -z "$username" ]; then
+  echo "user not found."
+  exit 1
+fi
+sudo usermod -L $username
+echo "block $username ."
+sudo pkill -KILL -u $username
 
