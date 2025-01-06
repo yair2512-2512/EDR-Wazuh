95bbe3fc4e87d12b19fe1f53ca199195ba8e60fd
yair2512-2512 <ycohen522@gmail.com>
2025-01-06 16:44:38 +0200

    Update scripts.sh

--- wazuh/scripts.sh
+++ wazuh/scripts.sh
@@ -27,4 +27,18 @@ for ip in $ips; do
   echo "IP $ip has been blocked from connecting."
 done
 ########################################
+###block_usb.sh###
+device=$1
+if [ -b "/dev/$device" ]; then
+    umount /dev/$device
+    echo "USB device $device has been unmounted."
+    chmod 000 /dev/$device
+    echo "USB device $device has been blocked permanently."
+else
+    echo "Device $device not found."
+fi
+########################################
+
+
+
 
