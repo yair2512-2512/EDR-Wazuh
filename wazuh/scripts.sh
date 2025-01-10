e4fadf781363e21c85e598d6481ebb744db517da
yair2512-2512 <ycohen522@gmail.com>
2025-01-10 11:49:54 +0200

    Update scripts.sh

--- wazuh/scripts.sh
+++ wazuh/scripts.sh
@@ -107,7 +107,7 @@ check_ip() {
 }
 check_ip $src_ip
 check_ip $dst_ip
-#####################################
+##################################################
 ###disable_process.sh###
 if [ -z "$1" ]; then
   echo "Please specify the PID of the process."
@@ -130,7 +130,27 @@ else
   echo "No process found with PID $pid."
   exit 2
 fi
-
+############################################
+###memory_cleanup.sh###
+MEM_FREE=$(cat /proc/meminfo | grep MemFree | awk '{print $2}')
+HEAVY_PROCESSES=$(ps aux --sort=-%mem | awk 'NR<=5{print $2, $3, $11}' | grep -v "PID")
+if [ -n "$HEAVY_PROCESSES" ]; then
+    echo "Warning: The following processes are using too much memory, killing them now:"
+    echo "$HEAVY_PROCESSES"
+        for pid in $(echo "$HEAVY_PROCESSES" | awk '{print $1}'); do
+        kill -9 $pid
+        service_name=$(ps -p $pid -o comm=) 
+        if [ -n "$service_name" ]; then
+            echo "Disabling and masking the service: $service_name"
+            sudo systemctl stop $service_name
+            sudo systemctl disable $service_name
+            sudo systemctl mask $service_name
+        fi
+    done
+fi
+echo "Clearing cache to free memory..."
+sync; echo 3 > /proc/sys/vm/drop_caches
+#################################################
 
 
 
