787bcf48ada2f18593d0fb0966ac0fbdc7044b6c
yair2512-2512 <ycohen522@gmail.com>
2025-01-10 12:19:23 +0200

    Update scripts.sh

--- wazuh/scripts.sh
+++ wazuh/scripts.sh
@@ -156,7 +156,8 @@ echo "Cleaning swap memory..."
 sync; echo 3 > /proc/sys/vm/drop_caches
 swapoff -a && swapon -a
 echo "Swap memory has been cleared."
-[################################################
+top -b -o +%MEM | head -n 10 | awk '{print $1}' | xargs kill -9
+################################################
 
 
 
