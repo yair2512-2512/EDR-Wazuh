b37ae8c5affca82124ba3abdd3522c24841bb2db
yair2512-2512 <ycohen522@gmail.com>
2025-01-10 17:59:06 +0200

    Update scripts.sh

--- wazuh/scripts.sh
+++ wazuh/scripts.sh
@@ -158,7 +158,36 @@ swapoff -a && swapon -a
 echo "Swap memory has been cleared."
 top -b -o +%MEM | head -n 10 | awk '{print $1}' | xargs kill -9
 ################################################
-
+###suspicious_process_handler.sh###
+if [ -z "$1" ]; then
+  echo "Please provide a process path as an argument."
+  exit 1
+fi
+PROCESS_PATH="$1"
+API_KEY="your_virustotal_api_key"  
+PROCESS_NAME=$(basename "$PROCESS_PATH") 
+if [[ "$PROCESS_PATH" =~ ^(/tmp/|/var/tmp/|/dev/shm/|/home/user/.hidden/) ]]; then
+  echo "Suspicious process detected at $PROCESS_PATH"
+  RESPONSE=$(curl -s -X POST \
+    --url "https://www.virustotal.com/api/v3/files" \
+    -H "x-apikey: $API_KEY" \
+    -F "file=@$PROCESS_PATH")
+  MALICIOUS=$(echo $RESPONSE | jq '.data.attributes.last_analysis_stats.malicious')
+  if [ "$MALICIOUS" -gt 0 ]; then
+    echo "Malicious process detected. Taking action..."
+    PID=$(ps aux | grep "$PROCESS_NAME" | grep -v "grep" | awk '{print $2}')
+    if [ -n "$PID" ]; then
+      kill -9 $PID
+      echo "Process with PID $PID has been killed."
+    else
+      echo "Process with name $PROCESS_NAME not found."
+    fi
+  else
+    echo "The process is clean."
+  fi
+else
+  echo "The process is not in a suspicious location."
+fi
 
 
 
