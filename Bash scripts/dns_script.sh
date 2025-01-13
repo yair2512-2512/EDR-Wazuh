22c75f737a50fb0ef56e52947acf34a7f2a627df
yair2512-2512 <ycohen522@gmail.com>
2025-01-13 21:34:19 +0200

    Update and rename dns-script.sh to dns_script.sh

--- Bash scripts/dns_script.sh
+++ Bash scripts/dns_script.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+# This script checks the reputation of a domain using the VirusTotal API and blocks the local machine if the domain is suspicious.
 API_KEY="YOUR_VIRUSTOTAL_API_KEY"
 URL="https://www.virustotal.com/api/v3/domains/"
+
 check_domain() {
     local domain=$1
     response=$(curl -s -X GET "$URL$domain" -H "x-apikey: $API_KEY")
@@ -12,11 +14,14 @@ check_domain() {
         echo "Domain $domain is safe."
     fi
 }
+
 block_local_machine() {
     local_ip=$(hostname -I | awk '{print $1}')
     echo "Blocking IP: $local_ip"
     sudo iptables -A INPUT -s $local_ip -j DROP
 }
+
 for domain in "$@"; do
     check_domain $domain
 done
+
