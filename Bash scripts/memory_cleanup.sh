7f735da104e068f8e453f2348124a15aea2b9fa5
yair2512-2512 <ycohen522@gmail.com>
2025-01-13 21:35:34 +0200

    Update memory_cleanup.sh

--- Bash scripts/memory_cleanup.sh
+++ Bash scripts/memory_cleanup.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+# This script checks for memory usage and terminates heavy processes, disables related services, and clears memory cache.
 MEM_FREE=$(cat /proc/meminfo | grep MemFree | awk '{print $2}')
 HEAVY_PROCESSES=$(ps aux --sort=-%mem | awk 'NR<=5{print $2, $3, $11}' | grep -v "PID")
+
 if [ -n "$HEAVY_PROCESSES" ]; then
     echo "Warning: The following processes are using too much memory, killing them now:"
     echo "$HEAVY_PROCESSES"
-        for pid in $(echo "$HEAVY_PROCESSES" | awk '{print $1}'); do
+    
+    for pid in $(echo "$HEAVY_PROCESSES" | awk '{print $1}'); do
         kill -9 $pid
         service_name=$(ps -p $pid -o comm=) 
         if [ -n "$service_name" ]; then
@@ -15,5 +18,6 @@ if [ -n "$HEAVY_PROCESSES" ]; then
         fi
     done
 fi
+
 echo "Clearing cache to free memory..."
 sync; echo 3 > /proc/sys/vm/drop_caches
