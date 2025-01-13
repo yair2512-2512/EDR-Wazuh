7120aba5f74ace3577cd720745bb926104662e26
yair2512-2512 <ycohen522@gmail.com>
2025-01-13 21:12:02 +0200

    Create block-port.sh

---
+++ Bash scripts/block-port.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+PORT=$1
+PIDS=$(fuser $PORT/tcp 2>/dev/null)
+if [ ! -z "$PIDS" ]; then
+    echo "Terminating process using port $PORT..."
+    for PID in $PIDS; do
+        kill -9 $PID
+    done
+fi
+echo "Blocking port $PORT..."
+ufw deny $PORT/tcp
+echo "Port $PORT is now blocked."
