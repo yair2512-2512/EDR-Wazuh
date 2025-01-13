6169e176e75db1dd819fbff60ce4e997f15f4d27
yair2512-2512 <ycohen522@gmail.com>
2025-01-13 21:37:52 +0200

    Update suspicious_process_handler.sh

--- Bash scripts/suspicious_process_handler.sh
+++ Bash scripts/suspicious_process_handler.sh
@@ -1,28 +1,30 @@
 #!/bin/bash
+# This script checks if a process is malicious using VirusTotal and kills it if necessary.
 if [ -z "$1" ]; then
   echo "Please provide a process path as an argument."
   exit 1
 fi
+
 PROCESS_PATH="$1"
 API_KEY="your_virustotal_api_key"  
 PROCESS_NAME=$(basename "$PROCESS_PATH") 
-  RESPONSE=$(curl -s -X POST \
-    --url "https://www.virustotal.com/api/v3/files" \
-    -H "x-apikey: $API_KEY" \
-    -F "file=@$PROCESS_PATH")
-  MALICIOUS=$(echo $RESPONSE | jq '.data.attributes.last_analysis_stats.malicious')
-  if [ "$MALICIOUS" -gt 0 ]; then
-    echo "Malicious process detected. Taking action..."
-    PID=$(ps aux | grep "$PROCESS_NAME" | grep -v "grep" | awk '{print $2}')
-    if [ -n "$PID" ]; then
-      kill -9 $PID
-      echo "Process with PID $PID has been killed."
-    else
-      echo "Process with name $PROCESS_NAME not found."
-    fi
+
+RESPONSE=$(curl -s -X POST \
+  --url "https://www.virustotal.com/api/v3/files" \
+  -H "x-apikey: $API_KEY" \
+  -F "file=@$PROCESS_PATH")
+
+MALICIOUS=$(echo $RESPONSE | jq '.data.attributes.last_analysis_stats.malicious')
+
+if [ "$MALICIOUS" -gt 0 ]; then
+  echo "Malicious process detected. Taking action..."
+  PID=$(ps aux | grep "$PROCESS_NAME" | grep -v "grep" | awk '{print $2}')
+  if [ -n "$PID" ]; then
+    kill -9 $PID
+    echo "Process with PID $PID has been killed."
   else
-    echo "The process is clean."
+    echo "Process with name $PROCESS_NAME not found."
   fi
 else
-  echo "The process is not in a suspicious location."
+  echo "The process is clean."
 fi
