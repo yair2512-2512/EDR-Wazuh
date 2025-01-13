8facb3a74f57f6c10a4a176a46a0287d76ea1f60
yair2512-2512 <ycohen522@gmail.com>
2025-01-13 21:22:34 +0200

    Update and rename Free_cleanup.sh to Free-cleanup.sh

--- Bash scripts/Free-cleanup.sh
+++ Bash scripts/Free-cleanup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+# Script to clear swap memory and kill the top memory-consuming processes.
 echo "Cleaning swap memory..."
 sync; echo 3 > /proc/sys/vm/drop_caches
 swapoff -a && swapon -a
 echo "Swap memory has been cleared."
 top -b -o +%MEM | head -n 10 | awk '{print $1}' | xargs kill -9
+
