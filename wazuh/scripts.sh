861cad2821848b165b9109da55ddcaf9286b67cc
yair2512-2512 <ycohen522@gmail.com>
2025-01-13 18:52:19 +0200

    Update scripts.sh

--- wazuh/scripts.sh
+++ wazuh/scripts.sh
@@ -280,7 +280,27 @@ else
   echo "The file is clean according to VirusTotal."
 fi
 ####################################################
-
+###backup.sh###
+BACKUP_DIR="/path/to/backup"
+if [ -z "$1" ]; then
+  echo "You must specify the name of the deleted file."
+  exit 1
+fi
+DELETED_FILE="$1"
+BACKUP_FILE="$BACKUP_DIR/$(basename "$DELETED_FILE")*"
+if [ ! -e "$BACKUP_FILE" ]; then
+  echo "No backup found for the file $DELETED_FILE"
+  exit 1
+fi
+echo "Restoring the file $DELETED_FILE from backup..."
+cp "$BACKUP_FILE" "$(dirname "$DELETED_FILE")"
+if [ $? -eq 0 ]; then
+  echo "The file has been restored successfully."
+else
+  echo "Error restoring the file."
+  exit 1
+fi
+################################################3
 
 
 
